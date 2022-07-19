5f8e30de348ffbcb46af17bc92c4f887203e8384
jmmyauh <meiyuki02211225@gmail.com>
2022-07-19 21:32:16 +0900

    制御構文1の課題

---
+++ 04/05_if.php
@@ -0,0 +1,20 @@
+<?php
+
+echo '$numの値を入力して下さい: ';
+$num = trim(fgets(STDIN));
+
+
+for ($i = 2; $i <=$num; $i++ ) {
+    if ($num % $i == 0 && $num != $i){
+        $msg = '素数ではありません';
+        break;
+    }
+    elseif($i = 1){
+        $msg = '素数ではありません';
+    }
+    else {
+        $msg = '素数です';
+    }
+}
+
+echo $msg;
