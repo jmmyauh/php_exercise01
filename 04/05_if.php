33e16ef5d21bc7a487895b408ad123e9b3c1a095
jmmyauh <meiyuki02211225@gmail.com>
2022-07-20 02:34:36 +0900

    04/05_if.phpを修正しました

--- 04/05_if.php
+++ 04/05_if.php
@@ -3,17 +3,18 @@
 echo '$numの値を入力して下さい: ';
 $num = trim(fgets(STDIN));
 
-
-for ($i = 2; $i <=$num; $i++ ) {
-    if ($num % $i == 0 && $num != $i){
-        $msg = '素数ではありません';
-        break;
-    }
-    elseif($i == 1){
-        $msg = '素数ではありません';
-    }
-    else {
-        $msg = '素数です';
+if ($num == 1) {
+    $msg = '素数ではありません';
+}
+else {
+    for ($i = 2; $i <=$num; $i++ ) {
+        if ($num % $i == 0 && $num != $i){
+            $msg = '素数ではありません';
+            break;
+        }
+        else {
+            $msg = '素数です';
+        }
     }
 }
 
