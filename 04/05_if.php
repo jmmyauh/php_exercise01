d9c000cea18d5f2a6b7a351be68562745d4ae046
jmmyauh <meiyuki02211225@gmail.com>
2022-07-20 05:58:58 +0900

    確認漏れのところ修正しました

--- 04/05_if.php
+++ 04/05_if.php
@@ -4,15 +4,14 @@ echo '$numの値を入力して下さい: ';
 $num = trim(fgets(STDIN));
 
 if ($num == 1) {
-    $msg = '素数ではありません';
+    $msg = $num . 'は素数ではありません';
 }
 else {
     for ($i = 2; $i <=$num; $i++ ) {
         if ($num % $i == 0 && $num != $i){
             $msg = '素数ではありません';
             break;
-        }
-        else {
+        } else {
             $msg = '素数です';
         }
     }
