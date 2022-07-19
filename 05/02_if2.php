a60bdb6cb6c5bf193d9dfc205ad874c6800ef404
jmmyauh <meiyuki02211225@gmail.com>
2022-07-20 06:08:20 +0900

    変数とインデント訂正しました

--- 05/02_if2.php
+++ 05/02_if2.php
@@ -3,11 +3,11 @@
 $poket_money = 1000;
 $fund_raising = 101;
 
-echo 'あなたの所持金は1000円です｡' . PHP_EOL;
+echo "あなたの所持金は{$poket_money}円です｡" . PHP_EOL;
 
 do {
     $poket_money = $poket_money - $fund_raising;
-    echo "101円募金しました｡" . PHP_EOL . "残り残高は{$poket_money}円です｡" . PHP_EOL;
+    echo "{$fund_raising}円募金しました｡" . PHP_EOL . "残り残高は{$poket_money}円です｡" . PHP_EOL;
 } while ($poket_money > $fund_raising);
 
 echo 'あなたはこれ以上募金できません｡';
--- 05/03_if2.php
+++ 05/03_if2.php
@@ -5,12 +5,9 @@ $foods = [
     '昼食' => 'うどん',
     '夕食' => 'ライス'
 ];
-echo '私は､'.PHP_EOL;
+echo '私は､' . PHP_EOL;
 
 foreach ($foods as $key => $value) {
-    echo "{$key}に{$value}" .PHP_EOL;
-
+    echo "{$key}に{$value}" . PHP_EOL;
 }
 echo 'を食べます';
-
-
