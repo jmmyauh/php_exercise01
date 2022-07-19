30eff33d4a5afbd4fc6fe504cd3001b8b3d24fe2
jmmyauh <meiyuki02211225@gmail.com>
2022-07-19 22:59:38 +0900

    制御構文2の課題

---
+++ 05/01_if2.php
@@ -0,0 +1,15 @@
+<?php
+
+$poket_money = 1000;
+$fund_raising = 101;
+
+
+echo 'あなたの所持金は1000円です｡' . PHP_EOL;
+
+while ($poket_money > $fund_raising) {
+    $poket_money = $poket_money - $fund_raising;
+
+    echo "101円募金しました｡" . PHP_EOL . "残り残高は{$poket_money}円です｡" . PHP_EOL;
+}
+
+echo 'あなたはこれ以上募金できません｡';
---
+++ 05/05_if2.php
@@ -0,0 +1,25 @@
+<?php
+
+$subjects = ['数学', '英語', '理科', '社会', '国語'];
+
+foreach ($subjects as $key) {
+    switch ($key) {
+    case '数学':
+        $days = "明日";
+        break;
+    case '英語':
+        $days = "明後日";
+        break;
+    case '理科':
+        $days = "明々後日";
+        break;
+    case '社会':
+        $days = "昨日";
+        break;
+    case '国語':
+        $days = "中止";
+        break;
+    }
+    echo "{$key}の試験は{$days}です｡" . PHP_EOL;
+}
+
